c4e00b460db9514c8d3d433c4bff025f9671a3b7
Naardennj <gabriel.angelis787@gmail.com>
2025-01-14 09:54:01 -0300

    Instalar o Angular CLI Importar o FormsModule Indentando  o codigo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,21 @@
-import { SucessoCadastroComponent } from './sucesso-cadastro/sucesso-cadastro.component';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { CadastroComponent } from './cadastro/cadastro.component';
-
+import { SucessoCadastroComponent } from "./sucesso-cadastro/sucesso-cadastro.component";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { CadastroComponent } from "./cadastro/cadastro.component";
 
 const routes: Routes = [
   {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'cadastro',
+    path: "",
+    pathMatch: "full",
+    redirectTo: "cadastro",
   },
   {
-    path: 'cadastro',
-    component: CadastroComponent
+    path: "cadastro",
+    component: CadastroComponent,
   },
   {
-    path: 'sucesso',
-    component: SucessoCadastroComponent
+    path: "sucesso",
+    component: SucessoCadastroComponent,
   },
 ];
 
--- src/app/sucesso-cadastro/sucesso-cadastro.component.ts
+++ src/app/sucesso-cadastro/sucesso-cadastro.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-sucesso-cadastro',
-  templateUrl: './sucesso-cadastro.component.html',
-  styleUrls: ['./sucesso-cadastro.component.css']
+  selector: "app-sucesso-cadastro",
+  templateUrl: "./sucesso-cadastro.component.html",
+  styleUrls: ["./sucesso-cadastro.component.css"],
 })
 export class SucessoCadastroComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
